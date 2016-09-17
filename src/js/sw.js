7a7694e96fc7e1e9c2a0e352133e9d595e53c65a
Eduardo Matos <eduardoj.matos@gmail.com>
2016-09-17 12:44:38 -0300

    buil path

--- src/js/sw.js
+++ src/js/sw.js
@@ -10,7 +10,7 @@ self.addEventListener("activate", function(event) {
 
 	caches.open('abcdev-2016-v1').then(function(cache) {
 	  return cache.addAll([
-      'https://eduardojmatos.github.io/abc-dev-2016/',
+      'https://eduardojmatos.github.io/abc-dev-2016/build/',
       '../index.html',
       '../obrigado.html',
       '../codigo-de-conduta.html',
